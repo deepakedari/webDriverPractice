f5fed9b88d0d1179ef56d12e196b735d617263fb
Mandar <Mandar@DESKTOP-E1NQGHF.attlocal.net>
2025-03-16 16:13:48 -0400

    Added a new test case- ReverseSring.java

---
+++ src/test/java/webDriverPractice/ReverseString.java
@@ -0,0 +1,16 @@
+package webDriverPractice;
+
+public class ReverseString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String strRev ="I am a tester";
+				for(int i=strRev.length()-1;i>=0;i--)
+				{
+					System.out.print(strRev.charAt(i));
+					
+				}
+	}
+
+
+}
